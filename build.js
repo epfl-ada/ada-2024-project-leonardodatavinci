7f68988650852f61a8dbc533d1c81b2799957a40
cmaximilian <cm.nuesperling@gmail.com>
2024-12-18 15:07:12 +0100

    Restructure repository.

---
+++ build.js
@@ -0,0 +1,89 @@
+const fs = require('fs');
+const { JSDOM } = require('jsdom');
+const postcss = require('postcss');
+const cssnano = require('cssnano');
+const PurgeCSS = require('purgecss').PurgeCSS;
+const critical = require('critical');
+
+// Load the HTML file
+const html = fs.readFileSync('src/main.html', 'utf8');
+const dom = new JSDOM(html);
+const document = dom.window.document;
+
+// Define the labelRefs function
+function labelRefs() {
+  let refs = document.querySelectorAll('a[href^="#fig"], a[href^="#table"]');
+  let figures = document.querySelectorAll('figure');
+  figures.forEach((figure, index) => {
+    let caption = figure.querySelector('figcaption');
+    let innerHTMLWithoutSpan = caption.innerHTML.replace(/<span>.*?<\/span>/, '');
+    caption.innerHTML = `<span>Figure ${index + 1}</span>${innerHTMLWithoutSpan}`;
+    refs.forEach(ref => {
+      if (ref.getAttribute('href').substring(1) == figure.id) {
+        ref.textContent = `${index + 1}`;
+        ref.classList.add('fig-ref');
+      }
+    });
+  });
+//   let tables = document.querySelectorAll('table');
+//   tables.forEach((table, index) => {
+//     let caption = table.querySelector('caption');
+//     caption.innerHTML = `Table ${index + 1}: ${caption.innerHTML}`;
+//     refs.forEach(ref => {
+//       if (ref.getAttribute('href').substring(1) == table.id) {
+//         ref.textContent = `${index + 1}`;
+//       }
+//     });
+//   });
+}
+
+// Execute the labelRefs function
+labelRefs();
+
+// Save the modified HTML back to the file
+fs.writeFileSync('index.html', dom.serialize());
+
+// // Minify the CSS
+// const css = fs.readFileSync('src/css/main.css', 'utf8');
+// postcss([cssnano])
+//   .process(css, { from: 'assets/css/reduced.css', to: 'assets/css/reduced.min.css' })
+//   .then(result => {
+//     fs.writeFileSync('assets/css/reduced.min.css', result.css);
+//     if (result.map) {
+//       fs.writeFileSync('assets/css/reduced.min.css.map', result.map.toString());
+//     }
+//   });
+
+// Purge unused CSS
+const purgeCSSResults = new PurgeCSS().purge({
+  content: ['index.html'],
+  css: ['src/css/main.css'],
+  safelist: ['canvas', '.fig-ref'],
+});
+
+purgeCSSResults.then(result => {
+  const purgedCSS = result[0].css;
+
+  // Minify the CSS
+  postcss([cssnano])
+    .process(purgedCSS, { from: 'src/css/main.css', to: 'dist/css/style.min.css' })
+    .then(result => {
+      fs.writeFileSync('dist/css/style.min.css', result.css);
+      if (result.map) {
+        fs.writeFileSync('dist/css/style.min.css.map', result.map.toString());
+      }
+
+      // Inline critical CSS
+      critical.generate({
+        inline: true,
+        base: '.',
+        src: 'index.html',
+        css: ['dist/css/style.min.css'],
+        target: {
+          html: 'index.html',
+          css: 'dist/css/uncritical.css'
+        },
+        extract: true
+      });
+    });
+});
--- src/js/particles.js
+++ src/js/particles.js
@@ -1,114 +1,3 @@
-// import { loadPoissonDiscPlugin } from "https://cdn.jsdelivr.net/npm/@tsparticles/plugin-poisson-disc@3.7.1/+esm";
-
-let particles_config = {
-    id: "tsparticles",
-    options: {
-      fullScreen: {
-        enable: false
-      },
-      fpsLimit: 60,
-      interactivity: {
-        detectsOn: "canvas",
-        events: {
-          onDiv: [{
-            enable: true,
-            selectors: ".hero-title",
-            mode: "bounce",
-            type: "rectangle"
-          },
-          {
-            enable: true,
-            selectors: "#bubble-circle-div",
-            mode: "bounce",
-            type: "circle"
-          },],
-          onClick: {
-            enable: true,
-            mode: "repulse"
-          },
-          onHover: {
-            enable: true,
-            mode: "repulse"
-          },
-          resize: true
-        },
-      },
-      // poisson: {
-      //   enable: true
-      // },
-      particles: {
-        number: {
-          value: 350,
-          density: {
-            enable: true,
-          }
-        },
-        links: {
-          enable: true,
-          distance: 100,
-          width: 2,
-          color: {
-            value: "#ff00ff"
-          }
-        },
-        move: {
-          enable: true,
-          speed: 2,
-          outModes: {
-            default: "bounce"
-          }
-        },
-        size: {
-          value: 20
-        },
-        shape: {
-            type: "emoji",
-            options: {
-              emoji: {
-                value: ["🍺", "🍂", "🍃", "❄️", "☀️", "⏱️", "🍺", "🍺", "🍺"]
-              }
-            },
-        },
-      },
-      themes: [
-        {
-          name: "light",
-          default: {
-            value: true,
-            mode: "light",
-            auto: true
-          },
-          options: {
-            particles: {
-              links: {
-                color: {
-                  value: "#212529"
-                }
-              }
-            }
-          }
-        },
-        {
-          name: "dark",
-          default: {
-            value: true,
-            mode: "dark",
-            auto: true
-          },
-          options: {
-            particles: {
-              links: {
-                color: {
-                  value: "#ffffff"
-                }
-              }
-            }
-          }
-        },
-      ],
-    }
-};
-
 let particles_img_config = {
   id: "tsparticles",
   options: {
@@ -200,7 +89,6 @@ let particles_img_config = {
           options: {
             image: [
               {
-                // src: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Pixel_art_Wizard_Portrait.svg/1920px-Pixel_art_Wizard_Portrait.svg.png",
                 src: "./assets/img/beer_green.png",
                 width: 100,
                 height: 100
@@ -213,14 +101,7 @@ let particles_img_config = {
           ]
           },
       },
-      // collisions: {
-      //   enable: true,
-      //   mode: "bounce"
-      // }
     },
-    // poisson: {
-    //   enable: true
-    // },
     themes: [
       {
         name: "light",
@@ -260,14 +141,8 @@ let particles_img_config = {
   }
 };
 
-// particles_config.options.particles.links.color.value = "#0000ff";
-
 (async () => {
   await loadSlim(tsParticles);
-  //await loadFull(tsParticles);
   await tsParticles.load(particles_img_config).then((container) => {
-    // container.loadTheme("dark");
   });  
-  //await tsParticles.refresh();
 })();
-
